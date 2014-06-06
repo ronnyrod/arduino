3b58c4dac1b612c97816e4083bc4ad9441317555
ronnyrod <ronny.rodriguez@gmail.com>
2014-06-06 23:54:14 +0200

    Some comments

--- humidity/dht11-controller/dht11_controller.ino
+++ humidity/dht11-controller/dht11_controller.ino
@@ -1,65 +1,68 @@
 
 
-#include <dht.h>
+#include <dht.h> //Available at https://github.com/RobTillaart/Arduino/tree/master/libraries/DHTlib Thanks to https://github.com/RobTillaart
 
-dht DHT;
-int lastSensorState;
-String inputString = "";
-boolean cmdReceived = false;
+#define FIRMWARE_VERSION 1 //Firmaware version
+#define DHT11_PIN 5 //DTH11 data pin
+#define LED_PIN 13 //Indicator led
+#define H_MAX_PIN 6 //HIGH: Humidity over maximum offset | LOW: Humidity under maximum offset
+#define H_MIN_PIN 7 //HIGH: Humidity under minimun offset | LOW: Humidity over minimum offset
+#define T_MAX_PIN 8 //HIGH: Temperature over maximum offset | LOW: Temperature under maximum offset
+#define T_MIN_PIN 9 //HIGH: Temperature under minimun offset | LOW: Temperature over minimum offset
 
-#define FIRMWARE_VERSION 1
+dht DHT; //DHT11 driver
 
-String noMessage = "";
-String okHeader = "OK";
-String errHeader = "ERROR";
-String evtHeader = "EVENT";
-String separator = ":";
-//Errors
+int lastSensorState; //Last sensor state read ( 0: OK | <0: ERROR | check DTHLib Headers)
+String inputString = ""; //String received from serial line
+boolean cmdReceived = false; //Indicates command available to parse and process
+
+String noMessage = ""; //Constant.  Empty message trick
+//Message headers and separator
+String okHeader = "OK"; //Positive response
+String errHeader = "ERROR"; //Error response
+String evtHeader = "EVENT"; //Controller event -
+String separator = ":"; //Message separator 
+//Error message, codes and bodies
 String msgUnknownCmd = "unknown command";
 String cmdError = "ERR";
 int errCodeUnknownCmd = 0;
-//Commands
-String cmdReadHumidity = "READH";
-String cmdReadTemperature = "READT";
-String cmdReadSensorState = "READS";
+//Reading commands
+String cmdReadHumidity = "READH";  //Requests for humidity last reading
+String cmdReadTemperature = "READT"; //Requests for temperature last reading
+String cmdReadSensorState = "READS"; //requests for sensor state last reading
+String cmdVersion = "VERSION"; //Requests for firmware version
+String cmdInfo = "INFO"; //Requests for controller information 
+//Writting commands - 
 String cmdForceReadSensor = "FORCE";
-String cmdVersion = "VERSION";
-String cmdInfo = "INFO";
-//Humidity and temperature control 
-String cmdEnableHumCtrl = "ONH";
-String cmdDisableHumCtrl = "OFFH";
-String cmdEnableTemCtrl = "ONT";
-String cmdDisableTemCtrl = "OFFT";
-//parameter modification commmands
-String cmdSetMaxHumidity = "SETMAXH";
-String cmdSetMinHumidity = "SETMINH";
-String cmdSetMaxTemperature = "SETMAXT";
-String cmdSetMinTemperature = "SETMINT";
+//Writting commands - control management 
+String cmdEnableHumCtrl = "ONH"; //Enables humidity control
+String cmdDisableHumCtrl = "OFFH"; //Disables humidity control
+String cmdEnableTemCtrl = "ONT"; //Enables temperature control
+String cmdDisableTemCtrl = "OFFT"; //Disables temperature control
+//Writting commands - parameter modification commmands
+String cmdSetMaxHumidity = "SETMAXH"; //Sets maximum humidity offset
+String cmdSetMinHumidity = "SETMINH"; //Sets minimum humidity offset
+String cmdSetMaxTemperature = "SETMAXT"; //Sets maximum temperature offset
+String cmdSetMinTemperature = "SETMINT"; //Sets minimum temperature offset
 //Events
-String evtHumOutRange = "HUMIDITY";
-String msgHumOutRange = "Humidity out of range";
-String evtTempOutRange = "TEMPERATURE";
-String msgTempOutRange = "Temperature out of range";
+String evtHumOutRange = "HUMIDITY"; //Indicates humidity event
+String msgHumOutRange = "Humidity out of range"; //Message event
+String evtTempOutRange = "TEMPERATURE"; //Indicates temperature event
+String msgTempOutRange = "Temperature out of range"; //Message
 //Parameters
-long lastReadTime = 0;
-long interval = 2000; //2 seg
-boolean controlHumidity = true;
-boolean controlTemperature = true;
-int maxHumidity = 60;
-int minHumidity = 34;
-int maxTemperature = 37;
-int minTemperature = 26;
+long lastReadTime = 0; //Last time from sensor was asked (ms)
+long interval = 2000; //Reading periodicity (ms)
+boolean controlHumidity = true; //Flag to control humidity
+boolean controlTemperature = true; //Flag to control temperature
+int maxHumidity = 60; //Maximum humidity offset
+int minHumidity = 34; //Minimum humidity offset
+int maxTemperature = 37; //Maximum temperature offset
+int minTemperature = 26; //Minimum temperature offset
 
-#define DHT11_PIN 5
-#define LED_PIN 13
-#define H_MAX_PIN 6
-#define H_MIN_PIN 7
-#define T_MAX_PIN 8
-#define T_MIN_PIN 9
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600); //Configures serial port
+  pinMode(LED_PIN, OUTPUT);  
   pinMode(H_MAX_PIN, OUTPUT);
   pinMode(H_MIN_PIN, OUTPUT);
   pinMode(T_MAX_PIN, OUTPUT);
@@ -69,6 +72,7 @@ void setup()
 
 void loop()
 {
+  //Parses received command and process requested action
   if (cmdReceived) {
     if (cmdReadHumidity.equalsIgnoreCase(inputString)) {
       printMSG(okHeader,cmdReadHumidity,DHT.humidity,noMessage);
@@ -157,14 +161,23 @@ void loop()
  
   
 }
+/*
+Turn off Humidity control management
+*/
 void turnOffHumCtrl() {
   digitalWrite(H_MAX_PIN, LOW);
   digitalWrite(H_MIN_PIN, LOW);  
 }
+/*
+Turn off Temperature control management
+*/
 void turnOffTemCtrl() {
   digitalWrite(T_MAX_PIN, LOW);
   digitalWrite(T_MIN_PIN, LOW);  
 }
+/*
+Send formatted response to serial port
+*/
 void printMSG(String header,String command,int result,String message) {
   Serial.print(header);
   Serial.print(separator);
@@ -179,6 +192,9 @@ void printMSG(String header,String command,int result,String message) {
   }  
   
 }
+/*
+Build info message to answers INFO command
+*/
 String getInfo() {
   String output = String("interval:");
   output = output + interval;
@@ -202,13 +218,18 @@ String getInfo() {
   }
   return output;
 }
+/*
+Call DTHLib to read humidity and temperature from sensor DHT11
+*/
 void readSensor() {
   digitalWrite(LED_PIN, HIGH);
   lastSensorState = DHT.read11(DHT11_PIN);
   lastReadTime = millis();
   digitalWrite(LED_PIN, LOW);
 }
-
+/*
+Process incomming serial data
+*/
 void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read(); 
